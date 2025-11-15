6451efb5422d5d360e4a24a2efff433fcf4cba88
goekh <goekh@users.noreply.github.com>
2025-11-15 12:07:48 +0100

    Add Vercel Analytics for visitor tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.jsx'
 import './i18n/config'
@@ -8,6 +9,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
     <SpeedInsights />
   </React.StrictMode>,
 )
